cb670caa3c3f867c887a4c3ecc151cbb9966fe5d
vickydollas <vickybet35@gmail.com>
2026-04-19 23:13:33 +0100

    login

--- server/api/auth/register.post.ts
+++ server/api/auth/register.post.ts
@@ -18,5 +18,5 @@ export default defineEventHandler(async (event) => {
         username: username,
         password: hashedPassword
     }).returning()
-    return inserResult[0]
+    return inserResult
 })
